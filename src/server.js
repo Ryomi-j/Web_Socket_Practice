53855911321eebc2558873f929bf5c21832f289a
Kim Eunjeong <ryomi.j78@gmail.com>
2023-08-10 14:36:33 +0900

    :tada: Make WebSocket server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import http from "http";
+import express from "express";
+import WebSocket from "ws";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log("Listen on http://localhost:3000");
+
+const server = http.createServer(app); // http server에 접근
+
+const wss = new WebSocket.Server({server}) // http 서버 위에 webSocket server 생성
+
+server.listen(3000, handleListen)
