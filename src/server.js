10c717bda878cd20b2e81db04ce4ca4de4dd030e
Kim Eunjeong <ryomi.j78@gmail.com>
2023-08-24 15:34:49 +0900

    :memo: Chore: delete unnecessary codes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
 import express from "express";
-import { instrument } from "@socket.io/admin-ui";
-import { Server } from "socket.io";
+import { SocketIO } from "socket.io";
 
 const app = express();
 
@@ -14,63 +13,6 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log("Listen on http://localhost:3000");
 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
-
-instrument(wsServer, { auth: false });
-
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) publicRooms.push(key);
-  });
-
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-
-  socket.onAny((e) => console.log(`Socket Event: ${e}`));
-
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // roomName에만 전송
-    wsServer.sockets.emit("room_change", publicRooms()); // (방을 만들면) 모든 room에 전송
-  });
-
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
-  });
-
-  socket.on("disconnect", () => {
-    // 방을 나가면 모든 room에 전송
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
+const wsServer = SocketIO(httpServer)
 
 httpServer.listen(3000, handleListen);
